183f5d2b93fb4e0d8039bbeffe23ee38e8aa75aa
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-06-18 15:16:05 +0200

    Beginning of Windows Impersonation sample app

---
+++ sample/THNETII.WebServices.WindowsIdentityImpersonation.Sample/Impersonation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using THNETII.WebServices.WindowsImpersonation;
+
+namespace THNETII.WebServices.WindowsIdentityImpersonation.Sample
+{
+    internal static class Impersonation
+    {
+        internal static int id = 1;
+
+        public static Task Run(IHost host, WindowsIdentity identity)
+        {
+            var id = Interlocked.Increment(ref Impersonation.id);
+            var serviceProvider = host.Services;
+            var appLifetime = serviceProvider.GetRequiredService<IHostApplicationLifetime>();
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger(typeof(Impersonation));
+            var cancelToken = appLifetime.ApplicationStopping;
+
+            var eventId = new EventId(id, identity.Name);
+            return WindowsIdentityAsync.RunImpersonatedAsync(identity.AccessToken,
+                () => RunImpersonated(logger, eventId, cancelToken));
+        }
+
+        private static async Task RunImpersonated(ILogger logger, EventId eventId, CancellationToken cancelToken)
+        {
+            while (!cancelToken.IsCancellationRequested)
+            {
+                var id = WindowsIdentity.GetCurrent(ifImpersonating: true);
+                if (id is null)
+                {
+                    logger.Log(LogLevel.Error, new EventId(-eventId.Id, eventId.Name), $"Expected impersonated windows identity, but got (null)");
+                    continue;
+                }
+
+                logger.Log(LogLevel.Information, eventId, $"Current User: {{{nameof(id.Name)}}}, Impersonation: {{{nameof(id.ImpersonationLevel)}}}", id.Name, id.ImpersonationLevel);
+
+                try { await Task.Delay(TimeSpan.FromSeconds(5), cancelToken).ConfigureAwait(false); }
+                catch (OperationCanceledException) { break; }
+            }
+        }
+    }
+}
---
+++ sample/THNETII.WebServices.WindowsIdentityImpersonation.Sample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace THNETII.WebServices.WindowsIdentityImpersonation.Sample
+{
+    public static class Program
+    {
+        internal static readonly object consoleSync = new object();
+
+        public static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .Build();
+
+            using (host)
+            {
+                await host.StartAsync().ConfigureAwait(false);
+
+                await Run(host, args).ConfigureAwait(false);
+
+                await host.StopAsync().ConfigureAwait(false);
+            }
+        }
+
+        private static async Task Run(IHost host, string[] args)
+        {
+            var serviceProvider = host.Services;
+            var appLifetime = serviceProvider.GetRequiredService<IHostApplicationLifetime>();
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger(typeof(Program));
+
+            var impersonationTasks = (args ?? Array.Empty<string>())
+                .Select(a => Impersonation.Run(host, a))
+                .ToList();
+
+            var cancelToken = appLifetime.ApplicationStopping;
+
+            while (!cancelToken.IsCancellationRequested)
+            {
+                var id = WindowsIdentity.GetCurrent();
+                logger.LogInformation($"Current User: {{{nameof(id.Name)}}}, Impersonation: {{{nameof(id.ImpersonationLevel)}}}", id.Name, id.ImpersonationLevel);
+                try { await Task.Delay(TimeSpan.FromSeconds(5), cancelToken).ConfigureAwait(false); }
+                catch (OperationCanceledException) { break; }
+            }
+
+            await Task.WhenAll(impersonationTasks).ConfigureAwait(false);
+        }
+    }
+}
