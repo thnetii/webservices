22a679bc14ec0978cad49ba0e29184c18dcb6223
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-11-17 11:26:54 +0100

    Get started with In-memory auth handler project

---
+++ src/THNETII.WebServices.Authentication.Memory/RemoteAuthenticationMemorySignInHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace THNETII.WebServices.Authentication.Memory
+{
+    public class RemoteAuthenticationMemorySignInHandler
+        : SignInAuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public RemoteAuthenticationMemorySignInHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock) { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            
+
+            throw new NotImplementedException();
+        }
+
+        protected override Task HandleSignInAsync(ClaimsPrincipal user, AuthenticationProperties properties)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+
+        protected override Task HandleSignOutAsync(AuthenticationProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
