4f2fb58d44421ddf41f48168d5854480f7f75f92
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-11 14:21:48 +0200

    Add RazorPages WebApp template

---
+++ template/THNETII.WebServices.Template.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace THNETII.WebServices.Template.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public static void OnGet()
+        {
+
+        }
+    }
+}
