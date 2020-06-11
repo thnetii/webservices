8a369e9f6a6b242565413cfd94022421936e8734
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-11 16:25:48 +0200

    Add individual auth WebApp template

---
+++ template/THNETII.WebServices.Template.WebAppIndividualAuth/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace THNETII.WebServices.Template.WebAppIndividualAuth.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public static void OnGet()
+        {
+
+        }
+    }
+}
